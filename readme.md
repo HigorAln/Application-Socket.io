cd0c81298016205056d4c032a003a4cafb77c1ba
Higor Allan <higor.allan21@gmail.com>
2022-01-25 10:26:31 -0300

    Readme.me

---
+++ readme.md
@@ -0,0 +1,17 @@
+## Chat Socket
+
+### Aplicação feita em nodejs com socket.io
+
+### Chat em realtime com sistema de notificacao por chat
+
+#### Tecnologias usadas:
+
+- Express
+- Mongoose
+- Socket.io
+- Uuid
+- Typescript
+- Ts-node-dev
+
+<img src="public/Captura de tela de 2022-01-25 10-22-11.png">
+<img src="public/Captura de tela de 2022-01-25 10-23-12.png">
--- src/http.ts
+++ src/http.ts
@@ -1,28 +1,28 @@
-import "reflect-metadata"
-import express from 'express'
-import path from 'path'
-import { createServer } from 'http'
-import { Server } from 'socket.io'
-import mongoose from 'mongoose'
+import "reflect-metadata";
+import express from "express";
+import path from "path";
+import { createServer } from "http";
+import { Server } from "socket.io";
+import mongoose from "mongoose";
 
 const app = express();
 
 const server = createServer(app);
 // #####################################################################################
-mongoose.connect("") // COLOCAR O URI DO MONGODB AQUI
+mongoose.connect(""); // COLOCAR O URI DO MONGODB AQUI
 
-app.use(express.static(path.join(__dirname, "..", "public")))
+app.use(express.static(path.join(__dirname, "..", "public")));
 
 const io = new Server(server);
 
-io.on("connection", (socket)=>{
-  console.log("Socket "+socket.id)
-})
+io.on("connection", (socket) => {
+  console.log("Socket " + socket.id);
+});
 
-app.get('/', (req,res)=>{
+app.get("/", (req, res) => {
   return res.json({
-    message: "Hello world"
-  })
-})
+    message: "Hello world",
+  });
+});
 
-export { server, io }
+export { server, io };
